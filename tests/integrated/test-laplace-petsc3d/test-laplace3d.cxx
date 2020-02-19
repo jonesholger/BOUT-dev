0d1cb60492ee81fd6b0ec846d3b01657b3bf858e
John Omotani <john.omotani@ukaea.uk>
2020-02-19 22:33:23 +0000

    Print backtrace from test-laplace-petsc3d

--- tests/integrated/test-laplace-petsc3d/test-laplace3d.cxx
+++ tests/integrated/test-laplace-petsc3d/test-laplace3d.cxx
@@ -29,101 +29,107 @@
 
 int main(int argc, char** argv) {
 
-  BoutInitialise(argc, argv);
+  try {
+    BoutInitialise(argc, argv);
 
-  ///////////////////////////////////////////////////////////////////////////////////////
-  // Initialise input
-  ///////////////////////////////////////////////////////////////////////////////////////
-  Field3D f, rhs;
-  initial_profile("rhs", rhs);
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Initialise input
+    ///////////////////////////////////////////////////////////////////////////////////////
+    Field3D f, rhs;
+    initial_profile("rhs", rhs);
 
-  // initial profile of f only used to set boundary values
-  initial_profile("f", f);
-  // Copy boundary values into boundary cells
-  for (auto it = mesh->iterateBndryLowerY(); !it.isDone(); it.next()) {
-    int x = it.ind;
-    int y = mesh->ystart - 1;
-    if (x == mesh->xstart) {
+    // initial profile of f only used to set boundary values
+    initial_profile("f", f);
+    // Copy boundary values into boundary cells
+    for (auto it = mesh->iterateBndryLowerY(); !it.isDone(); it.next()) {
+      int x = it.ind;
+      int y = mesh->ystart - 1;
+      if (x == mesh->xstart) {
+        for (int z = mesh->zstart; z <= mesh->zend; z++) {
+          f(x-1, y, z) = 0.5*(f(x-1, y - 1, z) + f(x-1, y, z));
+        }
+      }
       for (int z = mesh->zstart; z <= mesh->zend; z++) {
-	f(x-1, y, z) = 0.5*(f(x-1, y - 1, z) + f(x-1, y, z));
+        f(x, y, z) = 0.5*(f(x, y, z) + f(x, y + 1, z));
+      }
+      if (x == mesh->xend) {
+        for (int z = mesh->zstart; z <= mesh->zend; z++) {
+          f(x+1, y, z) = 0.5*(f(x+1, y - 1, z) + f(x+1, y, z));
+        }
       }
     }
-    for (int z = mesh->zstart; z <= mesh->zend; z++) {
-      f(x, y, z) = 0.5*(f(x, y, z) + f(x, y + 1, z));
-    }
-    if (x == mesh->xend) {
+    for (auto it = mesh->iterateBndryUpperY(); !it.isDone(); it.next()) {
+      int x = it.ind;
+      int y = mesh->yend + 1;
+      if (x == mesh->xstart) {
+        for (int z = mesh->zstart; z <= mesh->zend; z++) {
+          f(x-1, y, z) = 0.5*(f(x-1, y - 1, z) + f(x-1, y, z));
+        }
+      }
       for (int z = mesh->zstart; z <= mesh->zend; z++) {
-	f(x+1, y, z) = 0.5*(f(x+1, y - 1, z) + f(x+1, y, z));
+        f(x, y, z) = 0.5*(f(x, y - 1, z) + f(x, y, z));
+      }
+      if (x == mesh->xend) {
+        for (int z = mesh->zstart; z <= mesh->zend; z++) {
+          f(x+1, y, z) = 0.5*(f(x+1, y - 1, z) + f(x+1, y, z));
+        }
       }
     }
+    if (mesh->firstX()) {
+      int x = mesh->xstart - 1;
+      for (int y = mesh->ystart; y <= mesh->yend; y++) {
+        for (int z = mesh->zstart; z <= mesh->zend; z++) {
+          f(x, y, z) = 0.5*(f(x, y, z) + f(x + 1, y, z));
+        }
+      }
+    }
+    if (mesh->lastX()) {
+      int x = mesh->xend + 1;
+      for (int y = mesh->ystart; y <= mesh->yend; y++) {
+        for (int z = mesh->zstart; z <= mesh->zend; z++) {
+          f(x, y, z) = 0.5*(f(x - 1, y, z) + f(x, y, z));
+        }
+      }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Set up Laplace solver
+    ///////////////////////////////////////////////////////////////////////////////////////
+    auto laplace_solver = Laplacian::create();
+    Field3D A, C1, C2, D;
+    initial_profile("A", A);
+    initial_profile("C1", C1);
+    initial_profile("C2", C2);
+    mesh->communicate(C2);
+    C2.setBoundary("C2");
+    C2.applyParallelBoundary();
+    initial_profile("D", D);
+    laplace_solver->setCoefA(A);
+    laplace_solver->setCoefC1(C1);
+    laplace_solver->setCoefC2(C2);
+    laplace_solver->setCoefD(D);
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Solve
+    ///////////////////////////////////////////////////////////////////////////////////////
+    f = laplace_solver->solve(rhs, f);
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Calculate error
+    ///////////////////////////////////////////////////////////////////////////////////////
+    Field3D rhs_check = D*Laplace_perp(f) + Grad_perp(C2)*Grad_perp(f)/C1 + A*f;
+    Field3D error = rhs_check - rhs;
+    BoutReal error_max = max(abs(error), true);
+
+    SAVE_ONCE(f, rhs, rhs_check, error, error_max);
+    dump.write();
+
+    laplace_solver.reset(nullptr);
+  } catch (const BoutException& e) {
+    output << "Error encountered\n";
+    output << e.getBacktrace() << endl;
+    throw e;
   }
-  for (auto it = mesh->iterateBndryUpperY(); !it.isDone(); it.next()) {
-    int x = it.ind;
-    int y = mesh->yend + 1;
-    if (x == mesh->xstart) {
-      for (int z = mesh->zstart; z <= mesh->zend; z++) {
-	f(x-1, y, z) = 0.5*(f(x-1, y - 1, z) + f(x-1, y, z));
-      }
-    }
-    for (int z = mesh->zstart; z <= mesh->zend; z++) {
-      f(x, y, z) = 0.5*(f(x, y - 1, z) + f(x, y, z));
-    }
-    if (x == mesh->xend) {
-      for (int z = mesh->zstart; z <= mesh->zend; z++) {
-	f(x+1, y, z) = 0.5*(f(x+1, y - 1, z) + f(x+1, y, z));
-      }
-    }
-  }
-  if (mesh->firstX()) {
-    int x = mesh->xstart - 1;
-    for (int y = mesh->ystart; y <= mesh->yend; y++) {
-      for (int z = mesh->zstart; z <= mesh->zend; z++) {
-        f(x, y, z) = 0.5*(f(x, y, z) + f(x + 1, y, z));
-      }
-    }
-  }
-  if (mesh->lastX()) {
-    int x = mesh->xend + 1;
-    for (int y = mesh->ystart; y <= mesh->yend; y++) {
-      for (int z = mesh->zstart; z <= mesh->zend; z++) {
-        f(x, y, z) = 0.5*(f(x - 1, y, z) + f(x, y, z));
-      }
-    }
-  }
-
-  ///////////////////////////////////////////////////////////////////////////////////////
-  // Set up Laplace solver
-  ///////////////////////////////////////////////////////////////////////////////////////
-  auto laplace_solver = Laplacian::create();
-  Field3D A, C1, C2, D;
-  initial_profile("A", A);
-  initial_profile("C1", C1);
-  initial_profile("C2", C2);
-  mesh->communicate(C2);
-  C2.setBoundary("C2");
-  C2.applyParallelBoundary();
-  initial_profile("D", D);
-  laplace_solver->setCoefA(A);
-  laplace_solver->setCoefC1(C1);
-  laplace_solver->setCoefC2(C2);
-  laplace_solver->setCoefD(D);
-
-  ///////////////////////////////////////////////////////////////////////////////////////
-  // Solve
-  ///////////////////////////////////////////////////////////////////////////////////////
-  f = laplace_solver->solve(rhs, f);
-
-  ///////////////////////////////////////////////////////////////////////////////////////
-  // Calculate error
-  ///////////////////////////////////////////////////////////////////////////////////////
-  Field3D rhs_check = D*Laplace_perp(f) + Grad_perp(C2)*Grad_perp(f)/C1 + A*f;
-  Field3D error = rhs_check - rhs;
-  BoutReal error_max = max(abs(error), true);
-
-  SAVE_ONCE(f, rhs, rhs_check, error, error_max);
-  dump.write();
-
-  laplace_solver.reset(nullptr);
   BoutFinalise();
 
   return 0;
